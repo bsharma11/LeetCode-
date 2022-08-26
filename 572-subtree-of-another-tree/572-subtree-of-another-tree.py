fc354260e8c613eceec16dbc4cd2eca287f4e03c
Bhavtosh sharma <106946279+bsharma11@users.noreply.github.com>
2022-08-27 00:09:31 +0530

    Time: 1371 ms (5.04%), Space: 32.4 MB (12.59%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.py
@@ -5,20 +5,44 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, a, b) -> bool:
-        if a == None and b == None:
-            return True
-        if a == None or b == None:
-            return False
-        if a.val != b.val:
-            return False
-        return (self.isSameTree(a.left,b.left) and self.isSameTree(a.right,b.right))
-        
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if root:
-            if self.isSameTree(root, subRoot):
-                return True
-            else:
-                return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
-        else:
-            return False
+        print(root,subRoot)
+        self.a = []
+        def inorder1(node):
+            if node.left == None:
+                self.a.append(-1)
+            if node.right == None:
+                self.a.append(-2)
+            if node.left != None:
+                inorder1(node.left)
+            self.a.append(node.val)
+            if node.right != None:
+                inorder1(node.right)
+            
+            
+        self.c = []
+        def call(node):
+            if node == None:return
+            inorder1(node)
+            self.c.append(self.a)
+            self.a = []
+            call(node.left)
+            call(node.right)
+        call(root)
+        print(self.c)
+        
+        self.b = []
+        def inorder2(node):
+            if node.left == None:
+                self.b.append(-1)
+            if node.right == None:
+                self.b.append(-2)
+            if node.left != None:
+                inorder2(node.left)
+            self.b.append(node.val)
+            if node.right != None:
+                inorder2(node.right)
+        inorder2(subRoot)
+        print(self.b)
+        if self.b in self.c:return True
+        else:return False
